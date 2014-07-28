9623a4ce4fb93b0fc2787561fcd7334952cdf46f
Devaev Maxim <mdevaev@gmail.com>
2014-07-28 15:20:08 +0400

    test: using capsys

--- tests/test_typical_usage.py
+++ tests/test_typical_usage.py
@@ -10,52 +10,51 @@ import yaml
 
 # =====
 class TestTypicalUsage:
-    def test_usage(self):
-        with mock.patch("sys.stderr", new_callable=io.StringIO) as mock_stderr:
-            logging.config.dictConfig(yaml.load(textwrap.dedent("""
-                version: 1
-                disable_existing_loggers: false
-                loggers:
-                    test:
-                        level: DEBUG
-                        handlers: [default]
-                formatters:
-                    default:
-                        (): contextlog.MixedFormatter
-                        formatters:
-                            - colorlog.ColoredFormatter
-                            - contextlog.PartialFormatter
-                            - contextlog.ExceptionLocalsFormatter
-                        style: "{"
-                        format: "{log_color}{levelname:>7} {purple}{name:20.20}{reset} CTX={ctx} CTX_INT={ctx_internal} {message}"
-                handlers:
-                    default:
-                        level: DEBUG
-                        class: logging.StreamHandler
-                        formatter: default
-                root:
+    def test_usage(self, capsys):
+        logging.config.dictConfig(yaml.load(textwrap.dedent("""
+            version: 1
+            disable_existing_loggers: false
+            loggers:
+                test:
                     level: DEBUG
                     handlers: [default]
-            """)))
+            formatters:
+                default:
+                    (): contextlog.MixedFormatter
+                    formatters:
+                        - colorlog.ColoredFormatter
+                        - contextlog.PartialFormatter
+                        - contextlog.ExceptionLocalsFormatter
+                    style: "{"
+                    format: "{log_color}{levelname:>7} {purple}{name:20.20}{reset} CTX={ctx} CTX_INT={ctx_internal} {message}"
+            handlers:
+                default:
+                    level: DEBUG
+                    class: logging.StreamHandler
+                    formatter: default
+            root:
+                level: DEBUG
+                handlers: [default]
+        """)))
 
-            log = contextlog.get_logger(__name__, ctx="test")
-            log.info("Message #1")
+        log = contextlog.get_logger(__name__, ctx="test")
+        log.info("Message #1")
 
-            saved_logger = []  # Only for test!
+        saved_logger = []  # Only for test!
 
-            def method():
-                bar = 1
-                log = contextlog.get_logger(__name__, ctx_internal="method")
-                saved_logger.append(log)
-                log.debug("Message #2")
-                try:
-                    raise RuntimeError
-                except:
-                    log.exception("Exception")
-            method()
+        def method():
+            bar = 1
+            log = contextlog.get_logger(__name__, ctx_internal="method")
+            saved_logger.append(log)
+            log.debug("Message #2")
+            try:
+                raise RuntimeError
+            except:
+                log.exception("Exception")
+        method()
 
-            log = contextlog.get_logger(__name__)
-            log.info("Message #3")
+        log = contextlog.get_logger(__name__)
+        log.info("Message #3")
 
         output = (
             "\x1b[32m   INFO \x1b[35mtests.test_typical_u\x1b[39;49;0m CTX=test CTX_INT= "
@@ -67,7 +66,7 @@ class TestTypicalUsage:
             "Traceback (most recent call last):\n"
             "  File "
             "\"%s\", line "
-            "52, in method\n"
+            "51, in method\n"
             "    raise RuntimeError\n"
             "RuntimeError\n"
             "\n"
@@ -81,4 +80,4 @@ class TestTypicalUsage:
             "Message #3\x1b[39;49;0m\n"
         ) % (__file__, saved_logger[0], saved_logger[0])
 
-        assert mock_stderr.getvalue() == output
+        assert capsys.readouterr()[1] == output
