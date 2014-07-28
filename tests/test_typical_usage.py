8b55d795bf51cd7844f49c6a8c3117a673c42025
Devaev Maxim <mdevaev@gmail.com>
2014-07-28 18:06:37 +0400

    Using nonlocal

--- tests/test_typical_usage.py
+++ tests/test_typical_usage.py
@@ -29,12 +29,13 @@ def test_typical_usage(capsys, typical_usage_config, typical_usage_stderr):
     log = contextlog.get_logger(__name__, ctx="test")
     log.info("Message #1")
 
-    saved_logger = []  # Only for test!
+    saved_logger = None  # Only for test!
 
     def method():
         bar = 1
         log = contextlog.get_logger(__name__, ctx_internal="method")
-        saved_logger.append(log)
+        nonlocal saved_logger
+        saved_logger = log
         log.debug("Message #2")
         try:
             raise RuntimeError
@@ -46,4 +47,4 @@ def test_typical_usage(capsys, typical_usage_config, typical_usage_stderr):
     log.info("Message #3")
 
     captured_stderr = capsys.readouterr()[1]
-    assert captured_stderr == typical_usage_stderr % (__file__, saved_logger[0], saved_logger[0])
+    assert captured_stderr == typical_usage_stderr % (__file__, saved_logger, saved_logger)
