fb0efe52309205c5f0abc917f12fe99a92a04870
Devaev Maxim <mdevaev@gmail.com>
2014-09-17 17:29:15 +0400

    Bump version: 0.4 → 0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="contextlog",
-        version="0.4",
+        version="0.5",
         url="https://github.com/yandex-sysmon/contextlog",
         license="LGPLv3",
         author="Devaev Maxim",
